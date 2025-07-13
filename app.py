8a67c6781c7621119e8b7e43366258decd7b8275
nv23014-mohammed-taha <167678507+nv23014-mohammed-taha@users.noreply.github.com>
2025-07-13 21:18:05 +0300

    app.py

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/113L1QzGyfkDEx2eGmbAKDf6FVRvgPTz6
 """
 
-pip install streamlit
 
 import streamlit as st
 import pandas as pd
